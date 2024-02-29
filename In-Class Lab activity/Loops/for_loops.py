d76e0e6c63a371abc6cbd9e168d069a7acfb3448
mirzakulovs <mirzakulovs@hartwick.edu>
2024-02-29 10:02:17 -0500

    Create for_loops.py

---
+++ In-Class Lab activity/Loops/for_loops.py
@@ -0,0 +1,77 @@
+# 11.5.1.1
+for i in range(10):
+    print("Hi")
+
+
+# 11.5.1.2
+for i in range(5):
+    print("Hello")
+print("There")
+
+
+# 11.5.1.3
+for i in range(5):
+    print("Hello")
+    print("There")
+
+
+# 11.5.1.4
+for i in range(10):
+    print(i)
+
+
+# 11.5.1.5
+for i in range(1,11):
+    print(i)
+
+for i in range(10):
+    print(i+1)
+
+
+# 11.5.1.6
+for i in range(2, 12, 2):
+    print(i)
+
+for i in range(5):
+    print((i+1) * 2)
+
+
+# 11.5.1.7
+for i in range(10, 0, -1):
+    print(i)
+
+
+# 11.5.1.9
+for i in range(3):
+    print("a")
+for j in range(3):
+    print("b")
+
+
+# 11.5.1.11
+a = 0
+for i in range(10):
+    a = a + 1
+    for j in range(10):
+        a = a + 1
+print(a)
+
+
+# 11.5.1.12
+a = 0
+for i in range(10):
+    a = a + 1
+    for j in range(10):
+        a = a + 1
+print(a)
+
+
+# 11.5.1.13
+total = 0
+for i in range(1, 101):
+    total = total + 1
+print(total)
+
+
+for i in range(10):
+    print(i)
